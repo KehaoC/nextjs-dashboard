bac5eb553e489704dedfa934a71b8b990811e672
KehaoC <c13576076725@gmail.com>
2024-08-14 09:27:56 +0800

    Update search and invoices page.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,36 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce'
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback( (term) => {
+    // instead of creating complex literal string, using URLSearchParams API
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    console.log(`Searching...${term}`);
+
+    if (term) {
+      params.set('query', term);
+    }
+    else {
+      params.delete('query');
+    }
+    // It means that params are a complex object
+    console.log("Params: ", params.toString());
+
+    // After getting params
+    // ${pathname} is the current path
+    // params.toString() translates users' input into URL-friendly format
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +39,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={
+          (e) => {
+            handleSearch(e.target.value);
+          }
+        }
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
